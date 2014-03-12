3e52cdd53cd0f69feeeb5a8ec7b9b3c6ec8ee717
Chris Cowan <agentme49@gmail.com>
2014-03-12 12:06:00 -0700

    Remove extra stuff, fix warnings.

--- src/main/java/com/streak/objectifyperf/GuestbookServlet.java
+++ src/main/java/com/streak/objectifyperf/GuestbookServlet.java
@@ -27,6 +27,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class GuestbookServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
